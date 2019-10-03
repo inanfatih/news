ab1d6b0df52098834d1f9db4b86c411128cc3f59
inanfatih <mfatihinan@gmail.com>
2019-10-02 20:58:33 -0400

    data is fetched

--- script.js
+++ script.js
@@ -1,25 +1,53 @@
 let API_KEY = '48871837d14e4b66af953a3a30852a99';
 
-let country = '';
+let topHeadlines = 'https://newsapi.org/v2/top-headlines?';
 
-let topHeadlines =
-  'https://newsapi.org/v2/top-headlines?country=' +
-  country +
-  '&apiKey=' +
-  API_KEY;
+const endpointGenerator = (country, category, search) => {
+  country = country !== '' ? 'country=' + country : 'country=us';
+  //  sources !== '' ? (sources = '&sources=' + sources) : '&sources=bbc-news';
+  category = category !== '' ? '&category=' + category : '&category=business';
+  search = search !== '' ? '&q=' + search : search;
+
+  console.log('/////////', 'endpointgenerator');
+
+  let url = topHeadlines + country + category + search + '&apiKey=' + API_KEY;
+
+  console.log('==========', url);
+  return url;
+};
 
 function getNews(endpoint) {
   let xhr = new XMLHttpRequest();
-  let countOfNews = 0;
+  let ERROR_MESSAGE = 'Something bad happened!';
+  let data = [];
+
+  console.log('/////////', 'getnews');
 
   xhr.onreadystatechange = function() {
+    console.log('/////////', 'onreadystate');
+
+    let countOfNews = 0;
     if (xhr.readyState === 4) {
-      const data = JSON.parse(xhr.response);
+      data = JSON.parse(xhr.response);
       console.log(data);
 
-      if (data.status === 'ok') {
+      if (data.status === 'ok' && totalResults > 0) {
         countOfNews = data.totalResults;
+      } else {
+        document.body.innerHTML = ERROR_MESSAGE;
       }
     }
   };
+  xhr.open('GET', endpoint);
+  xhr.send();
 }
+
+document.getElementById('searchButton').addEventListener('click', function() {
+  console.log('/////////', 'button clicked');
+
+  let country = document.getElementById('country').value;
+  let category = document.getElementById('category').value;
+  let search = document.getElementById('search').value;
+
+  getNews(endpointGenerator(country, category, search));
+});
